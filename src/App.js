e83147f38364dab08a027d7cf516f3381bd4dda3
carl-larson <clarson08@gmail.com>
2020-05-19 20:51:00 -0500

    got the routing done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import Navigation from './components/Navigation'
+import Router from './Router'
 import './App.css'
 // Write imports for Router & BrowserRouter here //
 
 function App() {
   return (
-    <Navigation />
+    <BrowserRouter>
+      <Navigation />
+      <Router />
+    </BrowserRouter>
+    
   );
 }
-
 export default App;
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import cars from '../cars.json'
 // import material ui components here //
-// Container, Paper, Chip //
+import { Container, Paper, Chip } from '@material-ui/core'
 
 const Car = (props) => {
+    const id = props.match.params.id;
+    const carInfo = cars[id - 1];
     return (
-        <h1>A specific car</h1>
+        <Paper>
+            <Container>
+            <h1>A specific car: {id}</h1>
+            <h3>{carInfo.Name}</h3>
+            <Chip><span>Horsepower: {carInfo.Horsepower}</span></Chip>
+        </Container>
+        </Paper>
+        
+        
     )
 }
 
